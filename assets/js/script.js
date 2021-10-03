d0411a116e09594b7a094796fc29acf88a778840
Sophia Barrett <sophia@yoursummit.media>
2021-10-02 20:42:54 -0700

    refactor for legibility

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,14 @@ var generatePassword = function() {
     var inputLength = window.prompt("How many characters should your password have?");
     // validate user input
     if (inputLength < 8 || inputLength > 128 || isNaN(inputLength) || inputLength - Math.floor(inputLength) != 0) {
-      window.alert("Please choose a whole number between 8 and 128.");
+      window.alert("Please choose a number between 8 and 128.");
       return setLength();
     } else {
       return inputLength;
     };
   };
+
+  // store chosen password length in variable
   var length = setLength();
 
   // set available character types
@@ -49,36 +51,40 @@ var generatePassword = function() {
   ];
 
   // set character types to use in password based on user input
-  var chosenCharTypes = [];
-  var chooseCharTypes = function() {
+  var pickedCharTypes = [];
+  var pickCharTypes = function() {
     // loop through each item in charTypes and ask user to include or exclude
     for (var i = 0; i < charTypes.length; i++) {
       var include = window.confirm('Should your password include ' + charTypes[i].name +'? Select "OK" to include or "CANCEL" to exclude.');
-      // if user chooses to include, add to chosenCharTypes array
+      // if user chooses to include, add characters to pickedCharTypes array
       if (include) {
-        chosenCharTypes.push(charTypes[i].characters);
+        pickedCharTypes.push(charTypes[i].characters);
       }
     }
 
     // if user rejects all character types, reprompt
-    if (chosenCharTypes.length === 0) {
+    if (pickedCharTypes.length === 0) {
       window.alert("Please choose at least one type of character to include in your password.");
-      chooseCharTypes();
+      pickCharTypes();
     }
   };
-  chooseCharTypes();
 
-  // randomize each character of password one at a time
+  pickCharTypes();
+
+  // set password variable to empty
   var password = "";
+
+  // add a single random character to password, then repeat for chosen password length
   for (var i = 0; i < length; i++) {
-    // generate random number between 0 and the number of items in chosenCharTypes array
-    var charTypeSelector = Math.floor(Math.random() * chosenCharTypes.length);
-    // use random number to select set of characters based on index position in chosenCharTypes array
-    var selectedCharType = chosenCharTypes[charTypeSelector];
-    // generate random number between 0 and the number of characters in the selected item's string length
+    // generate random number between 0 and the number of items in pickedCharTypes array
+    var charTypeSelector = Math.floor(Math.random() * pickedCharTypes.length);
+    // use random number to select set of characters based on index position in pickedCharTypes array
+    var selectedCharType = pickedCharTypes[charTypeSelector];
+    // generate random number between 0 and the number of characters in the selected character's string length
     var charSelector = Math.floor(Math.random() * (selectedCharType.length));
     // use random number to select a single character from string and add it to end of password
     password += selectedCharType.charAt(charSelector);
   }
+  
   return password;
 };
